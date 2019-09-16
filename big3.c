fd3611ddf19d4dc3b778de311afc3d975655a63b
srikivn <55374614+srikivn@users.noreply.github.com>
2019-09-16 10:47:16 +0530

    Create big3.c
    big3.c added

---
+++ big3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void big3()
+{
+  int a,b,c;
+  printf("Enter the three numbers\n");
+  scanf("%d%d%d",&a,&b,&c);
+  if( a>b && a>c )
+    printf("%d is greater",a);
+  else if ( b>a && b>c )
+    printf("%d is greater",b);
+  else
+    printf("%d is greater",c);
+ }
