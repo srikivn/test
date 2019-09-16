78291dfcb8c8006658f144ff103ce9f96e9ae905
srikivn <55374614+srikivn@users.noreply.github.com>
2019-09-16 10:50:54 +0530

    Create fact.c
    fact.c is added

---
+++ fact.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void fact()
+{
+  int num,fact=1;
+  printf("\n\nEnter the num to find factorial\n");
+  scanf("%d",&num);
+  for(i=num;i>1;i--)
+    fact = fact*i;
+   printf("\nThe factorial of %d is %d\n",num,fact);
+}
