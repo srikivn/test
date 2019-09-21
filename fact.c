245e174eb4c833881e2f7d824bcf8d04ff6e7023
srikivn <55374614+srikivn@users.noreply.github.com>
2019-09-21 13:00:40 +0530

    Update fact.c

--- fact.c
+++ fact.c
@@ -4,6 +4,8 @@ void fact()
   int num,fact=1,i;
   printf("\n\nEnter the num to find factorial\n");
   scanf("%d",&num);
+  if(num==0 || num ==1)
+    fact=1;
   for(i=num;i>1;i--)
     fact = fact*i;
    printf("\nThe factorial of %d is %d\n",num,fact);
