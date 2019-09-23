c61eefcf75bbb72c3617ea0718ee759161c0bf88
srikivn <55374614+srikivn@users.noreply.github.com>
2019-09-23 18:48:43 +0530

    Update fact.c

--- fact.c
+++ fact.c
@@ -1,13 +1,11 @@
 /*C Program to find factorial of a number */
 #include<stdio.h>
-void fact()
+int fact()
 {
   int num,fact=1,i;
   printf("\n\nEnter the num to find factorial\n");
   scanf("%d",&num);
-  if(num==0 || num ==1)
-    fact=1;
   for(i=num;i>1;i--)
     fact = fact*i;
-   printf("\nThe factorial of %d is %d\n",num,fact);
+  printf("\nThe factorial of %d is %d\n",num,fact);
 }
